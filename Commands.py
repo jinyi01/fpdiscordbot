926af20c2a8638c6da01316989d2e922f24eb2fc
zek01 <39113059+zek01@users.noreply.github.com>
2019-02-13 19:20:56 -0800

    Add files via upload

---
+++ Commands.py
@@ -0,0 +1,47 @@
+
+COMMANDS = [
+    {
+        'name': 'Bot Help and Information', 
+        'description': 'Command: **?help**\nReturns with information about the bot and available commands.'
+        }, 
+    {
+        'name': 'Ping', 
+        'description': 'Command: **?ping**\nThe bot will return a message to check if it is online.\n'
+        }, 
+    {
+        'name': 'StockX Product Checker', 
+        'description': 'Command: **?stockx <search parameter>**\nSearches up the provided product name and gives a detailed breakdown on pricing and more.\nExample: `?stockx yeezy boost zebra`'
+        }, 
+    {
+        'name': 'StockX Fees Calculator', 
+        'description': 'Command: **?stockxfees <region> <transaction fee> <listing price>**\nRegions are "us", "ca", or "in" for international. Transaction fee is by percent (do not include processing fee). Calculates payout after StockX fees.\nExample: `?stockxfees ca 9.5 800`'
+        }, 
+    {
+        'name': 'Goat Product Checker', 
+        'description': 'Command: **?goat <search parameter>**\nSearches up the provided product name and gives a detailed breakdown on pricing and more.\nExample: `?goat yeezy boost bred`'
+        }, 
+    {
+        'name': 'Add To Cart Link Generator', 
+        'description': 'Command: **?atc <product url>**\nCreates ATC links for item on a shopify site and gives some information on the product.\nExample: `?atc https://someshopify.ca/product/some-product-url/`'
+        }, 
+    {
+        'name': 'Shopify Checker', 
+        'description': 'Command: **?shopifycheck <site url>**\nChecks if a website uses Shopify.\nExample: `?shopifycheck https://google.ca/`'
+        }, 
+    {
+        'name': 'Supreme Order Breakdown', 
+        'description': 'Command: **?supnycost <usd price>**\nGives a breakdown of the Canadian costs for Supreme. Includes USD prices.\nExample: `?supnycost 178`'
+        }, 
+    {
+        'name': 'Supreme Week', 
+        'description': 'Command: **?supweek**\nGives a link for the items of the latest supreme week.'
+        }, 
+    {
+        'name': 'Jigged Address Generator', 
+        'description': 'Command: **?addy <address>**\nGenerates 20 jigged adresses.\nExample: `?addy 123 FP Lane`'
+        }, 
+    {
+        'name': 'FP Sneakers Bot Commands', 
+        'description': 'Command: **?commands**\nGives information on proper command usage and a description on what each command does.'
+        }
+]
